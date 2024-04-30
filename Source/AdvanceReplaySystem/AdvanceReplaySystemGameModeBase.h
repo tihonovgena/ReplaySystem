12128e629d6e1e3c7b1011f58511b1df44a7ce09
Gennadii <>
2024-04-30 12:34:18 +0800

    Prepare CPP project

---
+++ Source/AdvanceReplaySystem/AdvanceReplaySystemGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AdvanceReplaySystemGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ADVANCEREPLAYSYSTEM_API AAdvanceReplaySystemGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
